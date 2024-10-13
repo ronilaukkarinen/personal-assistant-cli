84e66a6a77edb34d920ecc4e2883e35621206685
Roni Laukkarinen <roni@dude.fi>
2024-10-13 15:41:56 +0300

    Keep recurrence in recurring tasks

--- tasks/postpone.sh
+++ tasks/postpone.sh
@@ -9,12 +9,25 @@ postpone_task() {
     --header "Authorization: Bearer ${TODOIST_API_KEY}")
   task_name=$(echo "$task_data" | jq -r '.content')
 
-  # Update the task's due date
-  update_response=$(curl -s --request POST \
-    --url "https://api.todoist.com/rest/v2/tasks/$task_id" \
-    --header "Content-Type: application/json" \
-    --header "Authorization: Bearer ${TODOIST_API_KEY}" \
-    --data "{\"due_date\": \"$next_day\"}")
+  # If task is recurring
+  if [[ "$task_data" == *"recurring"* ]]; then
+    # Get current due_string for recurring tasks
+    current_due_string=$(echo "$task_data" | jq -r '.due_string')
+
+    # Update task's due date and keep recurrence
+    update_response=$(curl -s --request POST \
+      --url "https://api.todoist.com/rest/v2/tasks/$task_id" \
+      --header "Content-Type: application/json" \
+      --header "Authorization: Bearer ${TODOIST_API_KEY}" \
+      --data "{\"due_string\": \"$current_due_string\", \"due_date\": \"$next_day\"}")
+  else
+    # Update the task's due date
+    update_response=$(curl -s --request POST \
+      --url "https://api.todoist.com/rest/v2/tasks/$task_id" \
+      --header "Content-Type: application/json" \
+      --header "Authorization: Bearer ${TODOIST_API_KEY}" \
+      --data "{\"due_date\": \"$next_day\"}")
+  fi
 
   # Debug
   if [ "$DEBUG" = true ]; then
