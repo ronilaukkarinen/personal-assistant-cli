ee8d68920449990bee07030a13e0ba66a0c97db4
Roni Laukkarinen <roni@dude.fi>
2024-10-24 00:21:09 +0300

    Fix daily log

--- tasks/daily-log.sh
+++ tasks/daily-log.sh
@@ -17,15 +17,25 @@ daily_log() {
   # Use Finnish locale for the date
   export LC_TIME=fi_FI.UTF-8
 
-  # Date of today
-  today=$(date "+%Y-%m-%d")
+  # Determine the appropriate date command for macOS (Darwin) or other systems
+  if [[ "$(uname)" == "Darwin" ]]; then
+    date_cmd="gdate"
+  else
+    date_cmd="date"
+  fi
+
+
+  #today=$($date_cmd "+%Y-%m-%d")
+  today="2024-10-23"
+
+  echo "Processing daily log for $today..."
 
   # Lowercase month
-  month=$(date "+%B" | tr '[:upper:]' '[:lower:]')
+  month=$($date_cmd "+%B" | tr '[:upper:]' '[:lower:]')
 
-  # Header for the note in Finnish (e. g. Tiistai, 15. lokakuuta 2024)
-  weekday=$(date "+%A" | awk '{print toupper(substr($0,1,1)) tolower(substr($0,2))}')
-  header="$weekday, $(date "+%-d"). ${month}ta $(date "+%Y")"
+  # Header for the note in Finnish (e.g. Tiistai, 15. lokakuuta 2024)
+  weekday=$($date_cmd "+%A" | awk '{print toupper(substr($0,1,1)) tolower(substr($0,2))}')
+  header="$weekday, $($date_cmd "+%-d"). ${month}ta $($date_cmd "+%Y")"
 
   # Log file path (use your preferred location)
   log_file="$HOME/Documents/Brain dump/Päivittäinen reflektointi/$today.md"
@@ -40,16 +50,19 @@ daily_log() {
     # Extract task names without labels, ensure the task is completed today, and reverse the order
     task_info=$(echo "$completed_tasks" | jq -r --arg today "$today" '
       .items[] | select(.completed_at | startswith($today)) |
-      "- [x] \(.content | sub(" @.*"; "") ) (\(.labels | join(", "))) (valmis \(.completed_at | split("T")[1] | split(".")[0]))"
+      "- [x] \(.content | sub(" @.*"; "") ) (valmis \(.completed_at | split("T")[1] | split(".")[0]))"
     ' | while read -r line; do
-      completed_time=$(echo "$line" | grep -oP '(?<=valmis )[0-9:]+')
-      local_time=$(date -d "$completed_time UTC" +'%H:%M')
+      if [[ "$(uname)" == "Darwin" ]]; then
+        completed_time=$(echo "$line" | ggrep -oP '(?<=valmis )[0-9:]+')
+      else
+        completed_time=$(echo "$line" | grep -oP '(?<=valmis )[0-9:]+')
+      fi
+      local_time=$($date_cmd -d "$completed_time UTC" +'%H:%M')
       echo "$line" | sed "s/$completed_time/$local_time/"
     done | tac)
 
     # Count the number of completed tasks
     task_count=$(echo "$completed_tasks" | jq --arg today "$today" '[.items[] | select(.completed_at | startswith($today))] | length')
-
     # If $task count is 1, print "tehtävä", otherwise print "tehtävää"
     if [ "$task_count" -eq 1 ]; then
       task_label="tehtävä"
@@ -57,10 +70,10 @@ daily_log() {
       task_label="tehtävää"
     fi
 
-    # Add header to the log file with the task count
+    # Add header to the log file
     echo -e "# $header\n" > "$log_file"
     echo -e "## Tänään tehdyt asiat\n\n$task_info" >> "$log_file"
-    echo -e "\n\nYhteensä $task_count $task_label." >> "$log_file"
+    echo -e "Yhteensä $task_count $task_label." >> "$log_file"
     echo "Log saved to $log_file"
 
     # Debug
